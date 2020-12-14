6eb4f1a02b5eb95fc59c6df4e285f987cd57a11b
Raja Vyshnavi Sriramoju <rsriramoju@ucdavis.edu>
2020-12-14 10:20:28 -0800

    Adding testing script, results and pre-split dataset

---
+++ covid-gcn-fractions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#for seed in 1 2 3 4 5
+#for fraction in 0.01 0.02 0.4 0.6
+for fraction in 0.6 0.4
+do
+echo '******************** new seed ***************************'
+echo $seed
+    #for fraction in 0.01 0.02 0.05 0.1 0.2 0.4 0.6
+    for seed in 2 3 4 5
+    do
+    THEANO_FLAGS='device=cuda0,floatX=float32' nice -n 9 python -u gcnmain.py -hid 500 400 300 -bucket 120 -batch 500 -d ./data/covid-12k/  -mindf 14 -reg 0.0 -dropout 0.5 -cel 4 -highway -seed $seed -lblfraction $fraction -builddata -silent -maxdown 50
+
+
+    done
+done
+exit 0
