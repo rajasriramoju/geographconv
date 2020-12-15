2f12ac915636a8af2da0dbb78c95a883187551ef
Raja Vyshnavi Sriramoju <rsriramoju@ucdavis.edu>
2020-12-14 22:53:23 -0800

    README updated with small change to test script

--- covid-gcn-fractions.sh
+++ covid-gcn-fractions.sh
@@ -9,7 +9,7 @@ echo $seed
     #for fraction in 0.01 0.02 0.05 0.1 0.2 0.4 0.6
     for seed in 2 3 4 5
     do
-    THEANO_FLAGS='device=cuda0,floatX=float32' nice -n 9 python -u gcnmain.py -hid 500 400 300 -bucket 120 -batch 500 -d ./data/covid-12k/  -mindf 14 -reg 0.0 -dropout 0.5 -cel 4 -highway -seed $seed -lblfraction $fraction -builddata -silent -maxdown 50
+    THEANO_FLAGS='device=cuda0,floatX=float32' nice -n 9 python -u gcnmain.py -hid 500 400 300 -bucket 120 -batch 500 -d ./datasets/covid/  -mindf 14 -reg 0.0 -dropout 0.5 -cel 4 -highway -seed $seed -lblfraction $fraction -builddata -silent -maxdown 50
 
 
     done
